d89c530c4d537fb99c04e3586300009e09177206
abdallahokasha <okasha.fci@gmail.com>
2019-07-28 11:31:35 +0200

    Fix layout for the color picker.
    - CSS edits to fix layout for color-picker.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import Todos from './components/Todos';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Todos from "./components/Todos";
 class App extends Component {
   render() {
     // console.log(this.props);
     return (
       <Router>
-      <div className="App">
-        <h1 id="appTitle" className="leftPosition"> Todos App</h1>
-        <div>
-          <Route exact path="/" component={Signin} />
-          <Route exact path="/todos" component={Todos} />
-          <Route exact path="/signup" component={Signup} />
+        <div className="App mainDiv">
+          <h1 id="appTitle" className="leftPosition">
+            {" "}
+            Todos App
+          </h1>
+          <div>
+            <Route exact path="/" component={Signin} />
+            <Route exact path="/todos" component={Todos} />
+            <Route exact path="/signup" component={Signup} />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     );
   }
 }
 
 export default App;
-
